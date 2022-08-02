50c4ac2911b16c45fd720abcc041ad50f2418f82
Di0niz <moskovchenkody@gmail.com>
2022-08-02 09:27:00 +0300

    фикс настройки по умолчанию

--- src/enterprise/settings/local.py
+++ src/enterprise/settings/local.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ.get("DATABASE_NAME", "homework"),
         'USER': os.environ.get("DATABASE_USER", "user"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD", "random"),
-        'HOST': os.environ.get("DATABASE_HOST", "localhost"),
+        'HOST': os.environ.get("DATABASE_HOST", "store"),
         'PORT': os.environ.get("DATABASE_PORT", "5432"),
     }
 }
