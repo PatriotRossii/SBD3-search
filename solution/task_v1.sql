38fbaeb7121d0c3876fdee9d52beefee05abf088
PatriotRossii <patriotrossii2019@mail.ru>
2022-08-03 14:02:06 +0300

    Add solution for task_v1

--- solution/task_v1.sql
+++ solution/task_v1.sql
@@ -1,1 +1,11 @@
 -- Укажите дополнительные индексы
+
+CREATE INDEX id_idx ON users_user USING HASH (UPPER(id::text));
+CREATE INDEX first_name_idx ON users_user(UPPER(first_name::text) varchar_pattern_ops);
+CREATE INDEX last_name_idx ON users_user(UPPER(last_name::text) varchar_pattern_ops);
+
+-- DROP
+
+-- DROP INDEX IF EXISTS id_idx;
+-- DROP INDEX IF EXISTS first_name_idx;
+-- DROP INDEX IF EXISTS last_name_idx;
